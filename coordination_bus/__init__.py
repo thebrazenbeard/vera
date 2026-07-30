396321ac1fb71536acb3ca7a7cc1dbf7ef6c8abe
thebrazenbeard <patrickallynsims@gmail.com>
2026-07-30 19:54:48 -0400

    Expose one public coordination bus contract

--- coordination_bus/__init__.py
+++ coordination_bus/__init__.py
@@ -1,40 +1,38 @@
 """V.E.R.A. coordination bus v1 public interface."""
 
 from .contracts import (
-    ALL_PERMISSIONS, EVENT_STATUS_PAIRS, EVENT_TYPES, STATUSES, WORKSTREAMS,
-    ActorContext, CoordinationEvent, CoordinationEventDraft,
+    ACTOR_WORKSTREAM_ALIASES, ALL_PERMISSIONS, CANONICAL_MEMORY_ELIGIBLE,
+    EVENT_STATUS_PAIRS, EVENT_TYPES, INSTRUCTION_TRUST,
+    LEGACY_STORED_ADDRESSES, OBSOLETE_WORKSTREAMS, RECORD_CLASS, STATUSES,
+    WORKSTREAMS, ActorContext, CoordinationEvent, CoordinationEventDraft,
     CoordinationReceipt, CoordinationResult, PERMISSION_ACKNOWLEDGE,
-    PERMISSION_POST, PERMISSION_READ_ANY, PERMISSION_READ_SELF,
-    PERMISSION_RESOLVE, PERMISSION_REVIEW, PERMISSION_STATUS,
-    RepositoryConflict,
+    PERMISSION_DECIDE, PERMISSION_POST, PERMISSION_READ_ANY,
+    PERMISSION_READ_SELF, PERMISSION_RESOLVE, PERMISSION_REVIEW,
+    PERMISSION_STATUS, RepositoryConflict, canonical_hash, canonicalize,
+    classify_stored_address,
 )
-from .core import CoordinationBus as LegacyCoordinationBus
 from .in_memory import InMemoryCoordinationRepository
 from .supabase_sql import (
     GET_EVENT_SQL, INSERT_EVENT_SQL, LIST_THREAD_SQL, LIVE_SCHEMA_SNAPSHOT_V1,
     READ_INBOX_SQL, SqlExecutor, SupabaseSqlRepository,
 )
 from .temporal import (
-    CoordinationBus as LegacyTemporalCoordinationBus,
     TemporalCoordinationReceipt,
     TemporalCoordinationResult,
     TemporalEvidence,
 )
 from .verified_temporal import (
-    CoordinationBus as VerifierBoundCoordinationBus,
+    CoordinationBus,
     EVIDENCE_ENVELOPE_SCHEMA,
     HmacTemporalEvidenceAuthority,
     ReceiptTimeProvider,
     TemporalEvidenceEnvelope,
     TemporalEvidenceInput,
     TemporalEvidenceVerifier,
-    receipt_subject,
-)
-from .strict_temporal import (
-    CoordinationBus,
     acknowledgement_subject,
     entry_checkpoint_subject,
     exit_checkpoint_subject,
+    receipt_subject,
 )
 
 __all__ = [name for name in globals() if not name.startswith("_")]
